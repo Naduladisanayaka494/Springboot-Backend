5e4e47825dc87f601286ffdc8cfa465c112174e8
Naduladisanayaka494 <it20247140@my.sliit.lk>
2022-10-24 00:06:34 +0530

    first commit

---
+++ src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepo.java
@@ -0,0 +1,17 @@
+package tech.getarrays.employeemanager.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tech.getarrays.employeemanager.model.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee,Long>{
+	
+	
+	
+	void deleteEmployeeById(Long id);
+	Optional <Employee> findEmployeeById(Long id);
+	
+
+}
